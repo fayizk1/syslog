547234d14b1a9ffc0a7b3c7d3020be69b2210ee8
Vagrant User <vagrant@wheezy64-puppet3.vagrantup.com>
2015-02-16 11:05:20 +0000

    Added config; modfied example

--- example_server/main.go
+++ example_server/main.go
@@ -2,54 +2,111 @@ package main
 
 import (
 	"fmt"
-	"github.com/ziutek/syslog"
+	"github.com/fayizk1/syslog"
+	"github.com/robertkowalski/graylog-golang"
+	"code.google.com/p/gcfg"
 	"os"
 	"os/signal"
 	"syscall"
+	"strings"
 )
 
 type handler struct {
-	// To simplify implementation of our handler we embed helper
-	// syslog.BaseHandler struct.
 	*syslog.BaseHandler
 }
 
-// Simple fiter for named/bind messages which can be used with BaseHandler
-func filter(m *syslog.Message) bool {
-	return m.Tag == "named" || m.Tag == "bind"
+type Server struct {
+	Uri string
+	Grayloghostname string
+	Graylogport int
+}
+
+var server Server 
+
+type Filter struct {
+	Tag []string
+	Message []string
+}
+var filter Filter
+
+type ConfigReader struct {
+        Server struct {
+                Uri string
+		Grayloghostname string
+		Graylogport int
+        }
+	Filter struct {
+		Tag []string
+		Message []string
+	}
+}
+var cfgrd ConfigReader
+
+func init() {
+	err := gcfg.ReadFileInto(&cfgrd, "server.gcfg")
+	if err == nil {
+		server.Uri = cfgrd.Server.Uri
+		server.Grayloghostname = cfgrd.Server.Grayloghostname
+		server.Graylogport = cfgrd.Server.Graylogport
+		filter.Tag = cfgrd.Filter.Tag
+		filter.Message = cfgrd.Filter.Message
+	} else {
+		server = Server{"0.0.0.0:514", "127.0.0.1", 12201}
+//		filter =  Filter{[], []}
+		fmt.Println("Config Error:", err)
+	}
+	fmt.Println(filter.Message, filter.Tag)
+}
+
+func filterfn(m *syslog.Message) bool {
+	for i := range filter.Tag {
+		//fmt.Println(filter.Tag[i])
+		if m.Tag == filter.Tag[i] {
+			return false
+		}
+	}
+	for i:= range filter.Message {
+		if strings.Contains(m.Content, filter.Message[i]) {
+			return false
+		}
+	}
+	return true
 }
 
 func newHandler() *handler {
-	h := handler{syslog.NewBaseHandler(5, filter, false)}
-	go h.mainLoop() // BaseHandler needs some gorutine that reads from its queue
+	h := handler{syslog.NewBaseHandler(512, filterfn, false)}
+	go h.mainLoop() 
 	return &h
 }
 
-// mainLoop reads from BaseHandler queue using h.Get and logs messages to stdout
 func (h *handler) mainLoop() {
+	g := gelf.New(gelf.Config{
+		GraylogPort:     server.Graylogport,
+		GraylogHostname: server.Grayloghostname,
+		Connection:      "wan",
+	})
 	for {
 		m := h.Get()
 		if m == nil {
 			break
 		}
-		fmt.Println(m)
+		g.Log(m.Gelf())
+	//	fmt.Println(m)
+		//fmt.Println(m.Gelf())
 	}
 	fmt.Println("Exit handler")
 	h.End()
 }
 
 func main() {
-	// Create a server with one handler and run one listen gorutine
 	s := syslog.NewServer()
 	s.AddHandler(newHandler())
-	s.Listen("0.0.0.0:1514")
+	s.Listen(server.Uri)
 
-	// Wait for terminating signal
 	sc := make(chan os.Signal, 2)
 	signal.Notify(sc, syscall.SIGTERM, syscall.SIGINT)
 	<-sc
 
-	// Shutdown the server
 	fmt.Println("Shutdown the server...")
 	s.Shutdown()
 	fmt.Println("Server is down")
